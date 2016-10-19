2a796d10ca505646b3b312cdcf80e42110a0eb6f
Tomáš Lipovský <tomas.lipovsky@aponia.cz>
2016-10-19 13:37:24 +0200

    Cleanup Location model

--- src/ddr/ddr_models.py
+++ src/ddr/ddr_models.py
@@ -1,7 +1,8 @@
 from inspect import isclass
 from sqlalchemy import Column, Integer, ForeignKey, Text, Index
 from sqlalchemy.orm import relationship, backref
-from .database import Base
+from database import Base
+from models.location import Location
 
 def get_attribute(root, key, repeatable = False) :
     if isinstance(key, str) :
@@ -1243,41 +1244,3 @@ class DIVROUTE(Base) :
 
 
 
-
-# PERSONAL CLASS
-
-class Location(Base) :
-    __tablename__ = 'Location'
-    obj_id = Column(Integer, primary_key=True)
-
-    city = Column(Text)
-    village = Column(Text)
-    town = Column(Text)
-    hamlet = Column(Text)
-    house_number = Column(Text)
-    country = Column(Text)
-    postcode = Column(Text)
-    road = Column(Text)
-    osm_id = Column(Text)
-
-    def __init__(self, address=None, osm_id=None):
-        self.osm_id = osm_id
-
-        if 'city' in address:
-            self.city = address['city']
-        if 'village' in address:
-            self.village = address['village']
-        if 'town' in address:
-            self.town = address['town']
-        if 'hamlet' in address:
-            self.hamlet = address['hamlet']
-        if 'house_number' in address:
-            self.house_number = address['house_number']
-        if 'country' in address:
-            self.country = address['country']
-        if 'postcode' in address:
-            self.postcode = address['postcode']
-        if 'road' in address:
-            self.road = address['road']
-
-location_osm_id_index = Index('Location_osm_id_index', Location.osm_id)
