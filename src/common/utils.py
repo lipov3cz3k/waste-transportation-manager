8b735141c044c407919d0a6b0cf4e46d94096cd1
Tomáš Lipovský <tomas.lipovsky@aponia.cz>
2016-10-21 22:49:22 +0200

    add basic implementation of point to line distance

--- src/common/utils.py
+++ src/common/utils.py
@@ -263,3 +263,15 @@ def Season(now):
     now = now.replace(year=Y)
     return next(season for season, (start, end) in seasons
                 if start <= now <= end)
+
+def distancePointToLine(p0, p1, p2): # p3 is the point
+    x0, y0 = p0
+    x1, y1 = p1
+    x2, y2 = p2
+    try:
+        nom = abs((y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1)
+        denom = ((y2 - y1)**2 + (x2 - x1) ** 2) ** 0.5
+        result = nom / denom
+        return result
+    except ZeroDivisionError as e:
+        return 0
