bcdc9b9caad1f21f4958797db5f923480938f910
Tomáš Lipovský <tomas@lipovo.cz>
2018-01-14 01:13:16 +0100

    Streetnet segment <-> OSM way

--- src/models/cdv.py
+++ src/models/cdv.py
@@ -2,19 +2,18 @@ from database import Base, UniqueMixin
 from sqlalchemy import Column, Integer, Text, Float, Boolean
 
 
-class StreetnetSections(UniqueMixin, Base):
-    """ streetnet trasy """
-    __tablename__ = 'StreetnetSections'
+class StreetnetSegments(UniqueMixin, Base):
+    """ streetnet useky """
+    __tablename__ = 'StreetnetSegments'
 
     id = Column(Integer, primary_key=True) # ROAD_ID
-
     length = Column(Float) # delka
     time = Column(Float) # cas
     way_name = Column(Text) # silnice
     way_class = Column(Integer) # trida_kom
 
-    AADT_2010 = Column(Integer)
-    TV_2010 = Column(Integer)
+    AADT_2010 = Column(Integer) # Annual average daily traffic
+    TV_2010 = Column(Integer) # Truck vehicles
     bridge = Column(Boolean) # most
     underpass = Column(Boolean) # podjezd
     tunnel = Column(Boolean) # tunel
@@ -32,7 +31,15 @@ class StreetnetSections(UniqueMixin, Base):
 
     @classmethod
     def unique_filter(cls, query, **kwargs):
-        return query.filter(StreetnetSections.id == kwargs['id'])
+        return query.filter(StreetnetSegments.id == kwargs['id'])
 
     def __repr__(self):
         return '%s' % (self.id)
+
+class StreetnetOSMRelation(Base):
+    __tablename__ = 'StreetnetOSMRelation'
+
+    id = Column(Integer, primary_key=True)
+    version = Column(Integer)
+    streetnet_id = Column(Integer)
+    osm_way_id = Column(Integer)
